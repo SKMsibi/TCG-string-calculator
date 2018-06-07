0479e7ff76d98d9764f3c2a1d6276aa3a315361c
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-07 09:22:05 +0200

    add string calculator kata

--- 4-06-2018-string-calculator.js
+++ 4-06-2018-string-calculator.js
@@ -1,14 +1,18 @@
 function intAdd(arg) {
     var working = arg;
+    var negative = 0;
     var answer = 0;
     if (arg.search(/\/\//) >= 0) {
         var separator = arg[2];
-        working = arg.slice(arg.search(/\n/), arg.length)
+        working = arg.slice(arg.search(/\n/), arg.length);
+        var negative = working.search(/\-/);
         answer = working.length < 1 ? 0 : working.split(separator).reduce((a, b) => { return +a + +b }, 0);
     } else {
         while (working.search(/\n/) >= 0) { working = working.replace(/\n/, ","); }
+        negative = working.search(/\-/);
         answer = working.length < 1 ? 0 : working.split(',').reduce((a, b) => { return +a + +b }, 0)
     }
+
     return answer;
 }
 module.exports = { intAdd };
